936969cd41bdc0a264a8e3699ca1b1cd5cbdaead
TsuyosiUsugi <qesulive@gmail.com>
2023-09-26 17:37:48 +0900

    [update] マネージャの組み込み終わった

--- Assets/Usugi/Scripts/GameSceneManager.cs
+++ Assets/Usugi/Scripts/GameSceneManager.cs
@@ -13,6 +13,10 @@ public class GameSceneManager : MonoBehaviour
     [SerializeField] ReactiveProperty<GameState> _currentGameState = new (GameState.Prepare);
     [SerializeField] Text _startCountText;
     [SerializeField] ResultVIew _resultView;
+    [SerializeField] P1Command _p1Command;
+    [SerializeField] P2Command _p2Command;
+    [SerializeField] GameObject _p1CommandObj;
+    [SerializeField] GameObject _p2CommandObj;
 
     /// <summary> ステート一覧 </summary>
     public enum GameState
@@ -30,6 +34,10 @@ public class GameSceneManager : MonoBehaviour
 
     private void InitField()
     {
+        _p1Command.enabled = false;
+        _p2Command.enabled = false;
+        _p1CommandObj.SetActive(false);
+        _p2CommandObj.SetActive(false);
         _startCountText.gameObject.SetActive(false);
     }
 
@@ -45,11 +53,20 @@ public class GameSceneManager : MonoBehaviour
         await StartCount();
         Debug.Log("Start");
         _currentGameState.Value = GameState.Start;
+        ActiveCommand();
         Debug.Log("Playing");
         _currentGameState.Value = GameState.Playing;
         Debug.Log("End");
         _currentGameState.Value = GameState.End;
         EndSequence();
+    }
+
+    private void ActiveCommand()
+    {
+        _p1Command.enabled = true;
+        _p2Command.enabled = true;
+        _p1CommandObj.SetActive(true);
+        _p2CommandObj.SetActive(true);
     }
 
     /// <summary>
