98071db7038e6bb8b98720bb313e5ecf7b26ab6d
TsuyosiUsugi <qesulive@gmail.com>
2023-09-26 12:40:39 +0900

    [update] リザルト表示用のクラス作成

---
+++ Assets/Usugi/Scripts/ResultVIew.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ”»’è‚ğó‚¯æ‚Á‚ÄAŸ‚Á‚½•û‚ÉWIN,Lose•\¦‚·‚é
+/// ‚Ç‚ñ‚Ô‚è•\¦
+/// </summary>
+public class ResultVIew : MonoBehaviour
+{
+    
+}
